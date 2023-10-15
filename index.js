54450ee456deb45635840def68ee76414e21dc24
briankabuginjau <briannjau08@gmail.com>
2023-10-15 16:05:33 +0300

    Handling Errors When They Arise

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ app.get('/popularEnterprises', (req, res) => {
         res.status(200).json(documents);
       })
       .catch(error => {
-        res.status(200).json({ error: error.message });
+        res.status(500).json({ error: error.message });
       });
   });
 
