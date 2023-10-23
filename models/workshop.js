d21388612e84aa7ac080f9f44aba21cbb8f76457
briankabuginjau <briannjau08@gmail.com>
2023-10-24 02:23:26 +0300

    Updating Workshop Model

--- models/workshop.js
+++ models/workshop.js
@@ -10,7 +10,7 @@ const WorkshopSchema = new mongoose.Schema({
         required: true
     },
     date: {
-        type: Date,
+        type: String,
         required: true
     },
     location: {
