6f2bc2d3b200d2474811057c53a2cba59f2cdd23
briankabuginjau <briannjau08@gmail.com>
2023-12-01 00:54:13 +0300

    Adding Message Timestamps and Updating Model

--- models/message.js
+++ models/message.js
@@ -2,16 +2,23 @@ const mongoose = require('mongoose')
 
 const MessageSchema = new mongoose.Schema({
     sender: {
-        type: String,
-        required: true
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     },
     receiver: {
-        type: String,
-        required: true
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     },
     content: {
         type: String,
         required: true
+    },
+    timestamp: {
+        type: Date,
+        required: true,
+        default: Date.now
     }
 })
 
