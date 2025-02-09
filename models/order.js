1378fe6d4d02ed87767ab78da81c71c5eadbe949
briankabugi <briannjau01@gmail.com>
2025-02-09 15:55:46 +0300

    Adding Code to Order Schema

--- models/order.js
+++ models/order.js
@@ -29,6 +29,10 @@ const OrderSchema = new mongoose.Schema({
         type: String,
         required: false
     },
+    code: {
+        type: String,
+        required: false
+    },
     bids: {
         type: [],
         required: false
